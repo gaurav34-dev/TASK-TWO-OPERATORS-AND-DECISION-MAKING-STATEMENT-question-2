e88d1dc976c18124a3bdace4f190c6871c5321d3
gbaser54 <68541784+gbaser54@users.noreply.github.com>
2023-05-29 11:20:45 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    while(true)
+      {
+  System.out.println("Enter 1 - Addition");
+  System.out.println("Enter 2 - Subtraction");
+  System.out.println("Enter 3 - Division");
+  System.out.println("Enter 4 - Multiplication");
+  System.out.println("Enter 5 - Average");
+  Scanner s =new Scanner(System.in);
+    int m=s.nextInt();
+    if(m>0 && m<6)
+    {
+    System.out.println("enter 2 numbers");
+    int a=s.nextInt();
+    float b=s.nextInt();
+    int expression=m;
+   switch(expression) {
+  case 1:
+    System.out.println(a+b);
+    break;
+  case 2:
+    System.out.println(a-b);
+    break;
+  case 3:
+    System.out.println(a/b);
+    break;
+  case 4:
+    System.out.println(a*b);
+    break;
+  case 5:
+    System.out.println((a+b)/2.0);
+    break;
+  //default:
+    //System.out.println("negative number comming");
+    //break;
+   }
+}
+        else if(m<0){
+         System.out.println("negative number comming"); 
+        }
+        else
+          {
+          System.out.println("number is greater than 5 this option is not allowed to enter");
+          }
+}
+}
+}
+
+
+//swap two number without third variable
